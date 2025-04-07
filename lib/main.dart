314c762f4cd76b8674fffc5d8c8cf4cfc28c0939
Apple <apple@Apples-MacBook-Air.local>
2025-04-07 13:02:56 +0900

    AppBarの更新

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -23,7 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -32,85 +30,69 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static var _message = 'ok.'; //static 変数 _message を定義。アプリ内で表示するメッセージ。
+  static var _stars = '☆☆☆☆☆.';
+  static var _star = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // AppBar: アプリケーションの上部に表示されるタイトルバー
       appBar: AppBar(
-        title: Text('App Name'),
+        title: Text('My App'),
+        backgroundColor: Color(0xffa260bf), // ← ここで背景色を設定
+        leading: BackButton(color: Colors.white),
+        actions: <Widget>[
+          IconButton(
+            onPressed: iconPressedA,
+            icon: Icon(Icons.android),
+            tooltip: 'add star…',
+          ),
+          IconButton(
+            onPressed: iconPressedB,
+            icon: Icon(Icons.favorite),
+            tooltip: 'subtract star…',
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(30.0),
+          child: Center(
+            child: Text(
+              _stars,
+              style: TextStyle(fontSize: 22.0, color: Colors.white),
+            ),
+          ),
+        ),
       ),
       // Column: 画面の中央に縦に並べるウィジェット
       body: Center(
-        child: Column(
-          // mainAxisAlignment: 子ウィジェットの配置を縦方向（メイン軸）に調整
-          mainAxisAlignment: MainAxisAlignment.start,
-          // mainAxisSize: 子ウィジェットのサイズを縦方向（メイン軸）で最大化
-          mainAxisSize: MainAxisSize.max,
-          // crossAxisAlignment: 子ウィジェットの横方向（クロス軸）の配置を調整
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              // Padding: 内側の余白を設定。20.0の余白を周囲に追加
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                _message,
-                style: TextStyle(
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Roboto"),
-              ),
-            ),
-
-            Padding(
-              padding: EdgeInsets.all(10.0),
-            ),
-
-            Padding(
-                padding: EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                    onPressed:buttonPressed,
-                    child: Text(
-                      "tap me!",
-                      style: TextStyle(fontSize:32.0,
-                          color: const Color(0xff000000),
-                          fontWeight: FontWeight.w400,
-                          fontFamily: "Roboto"),
-                    )
-                )
-            ),
-          ],
+        child: Text(
+          _message,
+          style: const TextStyle(
+            fontSize: 28.0,
+          ),
         ),
       ),
     );
   }
 
-  void buttonPressed() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) =>
-          SimpleDialog(
-            title: const Text("Select assignment"),
-            children: <Widget>[
-              SimpleDialogOption(
-                child: const Text('One'),
-                onPressed: () => Navigator.pop<String>(context, 'One'),
-              ),
-              SimpleDialogOption(
-                child: const Text('Two'),
-                onPressed: () => Navigator.pop<String>(context, 'Two'),
-              ),
-              SimpleDialogOption(
-                child: const Text('Three'),
-                onPressed: () => Navigator.pop<String>(context, 'Three'),
-              ),
-            ],
-          ),
-    ).then<void>((value) => resultAlert(value));
+  void iconPressedA() {
+    _message = 'tap "android".';
+    _star++;
+    update();
   }
-  void resultAlert(String value) {
+
+  void iconPressedB() {
+    _message = 'tap "favorite".';
+    _star--;
+    update();
+  }
+
+  void update() {
+    _star = _star < 0 ? 0 : _star > 5 ? 5 : _star;
     setState(() {
-      _message = "selected: $value";
+      _stars = '★★★★★☆☆☆☆☆'.substring(5 - _star,
+      5 - _star + 5);
+      _message = _message + '[$_star]';
     });
   }
 }
