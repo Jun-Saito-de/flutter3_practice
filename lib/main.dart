c597d340a2a0924021bb89055633b6d150de2262
Apple <apple@Apples-MacBook-Air.local>
2025-04-09 09:55:22 +0900

    テキストと円の描画

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,81 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-// Flutterのアプリケーション全体をラップするStatelessWidget
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( //Flutterアプリの基本的な設定（テーマ、ナビゲーション、画面遷移）を管理
-      title: 'Generated App',
+    return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: const Color(0xff2196f3),
-        canvasColor: const Color(0xfffafafa),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MyHomePage(), // 最初に表示されるウィジェットを指定します。この場合、MyHomePage()というホームページが表示
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// State<MyHomePage>を継承
 class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255,255,255,255),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
-        title: const Text('App Name', style: TextStyle(fontSize: 30.0),
+        title: Text('App Name', style: TextStyle(fontSize: 30.0),),
+      ),
+      body:Container(
+        child: CustomPaint(
+          painter: MyPainter(),
         ),
       ),
-        body: Container(
-          child: CustomPaint(
-            painter: MyPainter(),
-          ),
-        ),
     );
   }
 }
 
-class MyPainter extends CustomPainter {
+class MyPainter extends CustomPainter{
 
   @override
-void paint(Canvas canvas, Size size){
+  void paint(Canvas canvas, Size size) {
     Paint p = Paint();
 
-    p.style = PaintingStyle.fill;
-    p.color = Color.fromARGB(150, 0, 200, 255);
-    Rect r = Rect.fromLTWH(50.0, 50.0, 150.0, 150.0);
-    canvas.drawRect(r, p);
+    ui.ParagraphBuilder builder = ui.ParagraphBuilder(
+      ui.ParagraphStyle(textDirection: TextDirection.ltr),
+    )
+      ..pushStyle(ui.TextStyle(color:Colors.red, fontSize: 48.0))
+      ..addText('Hello! ')
+      ..pushStyle(ui.TextStyle(color: Colors.blue[700],fontSize: 30.0))
+      ..addText('This is a sample of paragraph text. ')
+      ..pushStyle(ui.TextStyle(color: Colors.blue[200],fontSize: 30.0))
+      ..addText('You can draw MULTI-FONT text! ');
 
-    p.style = PaintingStyle.stroke;
-    p.color = Color.fromARGB(150, 200, 0, 255);
-    p.strokeWidth = 10.0;
-    r = Rect.fromLTWH(100, 100, 150, 150);
-    canvas.drawRect(r, p);
+    ui.Paragraph paragraph = builder.build()
+      ..layout(ui.ParagraphConstraints(width:300.0));
 
-    p.style = PaintingStyle.fill;
-    p.color = Color.fromARGB(150, 0, 150, 75);
-    Offset ctr = Offset(250.0, 350.0);
-    canvas.drawCircle(ctr, 50.0, p);
+    final double textWidth = paragraph.maxIntrinsicWidth;
+    final double textHeight = paragraph.height;
 
-    p.style = PaintingStyle.stroke;
-    p.color = Color.fromARGB(150, 55, 50, 255);
-    p.strokeWidth = 10.0;
-    r = Rect.fromLTWH(20, 270, 150, 200);
-    canvas.drawOval(r,p);
-
-    p.strokeWidth = 3.0;
-    for (var i = 0; i <= 10; i++){
-      Rect r = Rect.fromLTRB(
-          50.0 + 20 * i, 50.0,
-          50.0, 250.0 - 20 * i);
-      canvas.drawLine(r.topLeft, r.bottomRight, p);
-    }
+    Offset circleOffset = Offset(200.0, 200.0);
+    p.color = Color.fromARGB(100, 200, 205, 255);
+    canvas.drawCircle(circleOffset, 175.0, p);
+    Offset textOffset = Offset(70, 150.0 - 50.0);
+    canvas.drawParagraph(paragraph, textOffset);
   }
 
   @override
