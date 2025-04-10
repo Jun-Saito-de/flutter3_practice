252711dbc5176ea08c2b3f2a792c925eee1d5d0e
Apple <apple@Apples-MacBook-Air.local>
2025-04-10 12:15:05 +0900

    スライダーで円を調整

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,63 +28,86 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // Offset: x, y 座標を表すクラス
+  static double _value = 0;
+  static double _opaq = 0;
 
   @override
+  // 初期化処理
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  // 画面構築
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255,255,255,255),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
-        title: const Text("App Name", style: TextStyle(fontSize: 30.0),),
+        title: Text('App Name', style: TextStyle(fontSize: 30.0),),
       ),
-      body: Container(
-        child: CustomPaint(
-          painter:MyPainter(),
-        ),
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.all(10)),
+          Container(
+            width: 300,
+            height: 300,
+            child: CustomPaint(
+              painter: MyPainter(_value, _opaq.toInt()),
+              child: Center(),
+            ),
+          ),
+          Slider(
+            min: 0.0,
+            max: 300.0,
+            value: _value,
+            onChanged: _changeVal,
+          ),
+          Slider(
+            min: 0.0,
+            max: 255.0,
+            value: _opaq,
+            onChanged: _changeOpaq,
+          ),
+        ],
       ),
     );
   }
+
+  void _changeVal(double value) {
+    setState(() {
+      _value = value;
+    });
+  }
+  void _changeOpaq(double value) {
+  setState(() {
+    _opaq = value;
+  });
+}
 }
 
-class MyPainter extends CustomPainter{
+class MyPainter extends CustomPainter {
+  final double _value;
+  final int _opaq;
+
+  MyPainter(this._value, this._opaq);
+
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.translate(size.width / 2, size.height / 2);
-    Path path = Path();
-    Rect r = Rect.fromLTWH(50.0, 50.0, 75.0, 75.0);
-    path.addOval(r);
-    r = Rect.fromLTWH(75.0, 75.0, 125.0, 125.0);
-    path.addOval(r);
-    r = Rect.fromLTWH(125.0, 125.0, 175.0, 175.0);
-    path.addOval(r);
-
-    canvas.save();
-
     Paint p = Paint();
-    p.color = Color.fromARGB(150, 255, 0, 0);
+
     p.style = PaintingStyle.fill;
-    canvas.drawPath(path,p);
-
-    canvas.translate(0.0, 150.0);
-    p.color = Color.fromARGB(150, 0, 0, 255);
-    canvas.drawPath(path,p);
-
-    p.color = Color.fromARGB(150, 0, 255, 0);
-    canvas.rotate(-0.5 * pi);
-    canvas.translate(-500.0, 50.0);
-    canvas.scale(1 * 1.5);
-    canvas.drawPath(path,p);
-
-    canvas.restore();
-
-    Path triangle = Path();
-    triangle.moveTo(100, 100);
-    triangle.lineTo(150, 150);
-    triangle.lineTo(200, 100);
-    triangle.close(); // 三角形を閉じる
-    Paint tp = Paint();
-    tp.color = Color.fromARGB(250, 255, 250, 70);
-    tp.style = PaintingStyle.fill;
-    canvas.drawPath(triangle,tp);
+    p.color = Color.fromARGB(_opaq, 0, 200, 100);
+    Rect r = Rect.fromLTWH(
+        (size.width - _value) / 2,
+      (size.height - _value) / 2,
+      _value, _value);
+    canvas.drawOval(r, p);
+    
+    r = Rect.fromLTWH(0, 0, size.width,size.height);
+    p.style = PaintingStyle.stroke;
+    p.color = Color.fromARGB(255, 100, 100, 100);
+    canvas.drawRect(r, p);
   }
 
   @override
